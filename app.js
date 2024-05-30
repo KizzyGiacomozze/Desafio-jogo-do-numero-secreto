0f35aa2734c559cfe5973615135be4c95c62ae80
KizzyGiacomozze <kizzyalannigiacomozze@gmail.com>
2024-05-30 17:10:34 -0300

    Jogo do número secreto

---
+++ app.js
@@ -0,0 +1,26 @@
+alert("Boas vindas ao jogo do número secreto!");
+
+let numeroMaximo = 150;
+let numeroSecreto = Math.floor(Math.random() * numeroMaximo) + 1;  
+console.log(numeroSecreto);
+
+let chute;
+let tentativas = 1;
+
+while(chute != numeroSecreto){
+    chute = parseInt(prompt(`Escolha um número entre 1 e ${numeroMaximo}`), 10);
+
+    if (chute == numeroSecreto){
+        break;
+    }else{
+        if (chute > numeroSecreto){
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+    }
+    tentativas++;
+}
+
+let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+alert(`Parabéns! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
